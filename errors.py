a48c654f8414212ae18f451c9223dc15ea33dfda
Ayrton Hiroshi Sato <ayrton_sato@hotmail.com>
2023-02-07 16:52:34 -0300

    Error: LBYL + Race Condition

---
+++ errors.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+if os.path.exists("names.txt"):
+    print("O arquivo existe")
+    input("...") # Race Condition
+    names = open("names.txt").readlines()
+else:
+    print("[Error] File names.txt not found.")
+    sys.exit(1)
+
+# LBYL - Look Before You Leap
+if len(names) >= 3:
+    print(names[2])
+else:
+    print("Missing name in the list")
+    sys.exit(1)
