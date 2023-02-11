96e5f72cb48f00957e7330a610f2e24aad5a2d73
Ayrton Hiroshi Sato <ayrton_sato@hotmail.com>
2023-02-11 19:31:07 -0300

    refatorado para envio de e-mails

--- interpolacao.py
+++ interpolacao.py
@@ -3,6 +3,8 @@ __version__ = "0.1.1"
 
 import sys
 import os
+import smtplib
+from email.mime.text import MIMEText
 
 arguments = sys.argv[1:]
 if not arguments:
@@ -16,20 +18,25 @@ path = os.curdir
 filepath = os.path.join(path, filename)
 templatepath = os.path.join(path, templatename)
 
-clientes = []
-for line in open(filepath):
-    name, email = line.split(",")
-    # TODO: substituir por envio de email
-    print(f"Enviando email para: {email}")
-    print(
-        open(templatepath).read()
-        % {
-            "nome": name,
-            "produto": "caneta",
-            "texto": "Escrever muito bem",
-            "link": "https://canetaazul.com.br",
-            "quantidade": 1,
-            "preco": 10.00,
-        }
-    )
-    print("-" * 50)
+with smtplib.SMTP(host="localhost", port=8025) as server:
+    for line in open(filepath):
+        name, email = line.split(",")
+        text = (
+            open(templatepath).read()
+            % {
+                "nome": name,
+                "produto": "caneta",
+                "texto": "Escrever muito bem",
+                "link": "https://canetaazul.com.br",
+                "quantidade": 1,
+                "preco": 10.00,
+            }
+        )
+        from_ = "ayrton@hotmail.com"
+        to = ", ".join([email])
+        message = MIMEText(text)
+        message["Subject"] = "Compre mais"
+        message["From"] = from_
+        message["To"] = to
+
+        server.sendmail(from_, to, message.as_string())
