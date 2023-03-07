3d3e86a950cdc7f53093f36d3aafb9b3dcdc0411
Ayrton Hiroshi Sato <ayrton_sato@hotmail.com>
2023-03-07 12:25:48 -0300

    Resumo sobre 4 pilares de OO

--- oo/encapsulamento.py
+++ oo/encapsulamento.py
@@ -4,22 +4,34 @@
 # implementação se comportar
 # de maneira independente da forma do objeto
 
-# Encapsulamento
+# Encapsulamento: Capacidade de esconder/proteger
+# atributos dentro da classe, publico, protegidas, privadas
+from abc import ABC
 
-class Conta:
 
-    _tipo_de_conta = "corrente" # protegido / protected
-    __id__interno = 456789 # Privado
-
+class Conta(ABC):
     def __init__(self, cliente) -> None:
         self.cliente = cliente
-        if self._tipo_de_conta == "corrente":
-            self._saldo = 500
+        self._saldo = 500
 
+
+class ContaCorrente(Conta):
     def consultar(self):
         if self._saldo < 0:
             print("AVISO: Você está devendo...")
         return self._saldo
 
-conta = Conta(cliente="Ayrton")
+    @property
+    def saldo(self):
+        return self._saldo
+
+    @saldo.setter
+    def saldo(self, value):
+        self._saldo += value
+
+
+conta = ContaCorrente(cliente="Ayrton")
 print(conta.consultar())
+print(conta.saldo)
+conta.saldo = 100
+print(conta.saldo)
---
+++ oo/readme.md
@@ -0,0 +1,17 @@
+# Resumo dos 4 pilares de OO
+
+## Abstração
+
+Capacidade de abstrair implementações.
+
+## Herança
+
+Capacidade de herdar de outras classes.
+
+## Polimorfismo
+
+Capacidade de uma implementação se comportar de maneira independente da forma do objeto.
+
+## Encapsulamento
+
+Capacidade de esconder/proteger atributos dentro da classe, publico, protegidas, privadas.
